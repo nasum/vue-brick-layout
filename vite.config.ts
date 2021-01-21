0ed575b9decf16b60856a806f6aa70118b70fa80
nasum <tomato.wonder.life@gmail.com>
2021-01-21 22:54:21 +0900

    setting build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib', 'main.ts'),
+      name: 'VueBrickLayout'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   plugins: [vue()]
 })
